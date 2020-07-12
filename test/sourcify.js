6d6494a455cbe0ec6078a25bc1345dbbe02be27d
Jakov Buratovic <kraljipo007@gmail.com>
2020-07-12 13:18:25 +0200

    test order changed

--- test/sourcify.js
+++ test/sourcify.js
@@ -3,43 +3,46 @@ const chaiExec = require("@jsdevtools/chai-exec");
 
 chai.use(chaiExec);
 
-describe("CLI test", () => {
-    it("should ask for arguments", () => {
+describe("CLI test", function() {
+
+    this.timeout(15000);
+
+    it("should ask for arguments", function() {
         const myCLI = chaiExec('node dist/sourcify.js');
         chai.expect(myCLI).stderr.to.contain("Missing required arguments: chain, address");
         chai.expect(myCLI).exitCode.to.equal(1);
     })
-    it("should ask for address", () => {
+    it("should ask for address", function() {
         const myCLI = chaiExec('node dist/sourcify.js -c');
         chai.expect(myCLI).stderr.to.contain("Missing required argument: address");
         chai.expect(myCLI).exitCode.to.equal(1);
     })
-    it("should ask for chain", () => {
+    it("should ask for chain", function() {
         const myCLI = chaiExec('node dist/sourcify.js -a');
         chai.expect(myCLI).stderr.to.contain("Missing required argument: chain");
         chai.expect(myCLI).exitCode.to.equal(1);
     })
-    it("should show help", () => {
+    it("should show help", function() {
         const myCLI = chaiExec('node dist/sourcify.js --help');
         chai.expect(myCLI).to.have.stdout.that.matches(/^Options:\n\s*--help\s*Show help\s*\[boolean\]\n\s*--version\s*Show version number\s*\[boolean\]/);
         chai.expect(myCLI).exitCode.to.equal(0);
     })
-    it("should show version number", () => {
+    it("should show version number", function() {
         const myCLI = chaiExec('node dist/sourcify.js --version');
         chai.expect(myCLI).to.have.stdout.that.matches(/^\d{1,}.\d{1,}.\d{1,}/);
         chai.expect(myCLI).exitCode.to.equal(0);
     })
-    it("should pass verification (existing sourcecode)", () => {
-        const myCLI = chaiExec('node dist/sourcify.js -c 1 -a "0xfff0f5801a9e13426c306455A3BcC5EF3e9BC979"');
-        chai.expect(myCLI).to.have.stdout.that.contains("perfect");
-        chai.expect(myCLI).exitCode.to.equal(0);
-    })
-    it("should pass verification (upload files)", () => {
+    it("should pass verification (upload files)", function() {
         const myCLI = chaiExec('node dist/sourcify.js -c 1 -a "0xfff0f5801a9e13426c306455A3BcC5EF3e9BC979" -f "testcontracts/ERC20Standard.sol" "testcontracts/metadata.json"');
         chai.expect(myCLI).to.have.stdout.that.contains("perfect");
         chai.expect(myCLI).exitCode.to.equal(0);
     })
-    it("should say that address is not found", () => {
+    it("should pass verification (existing sourcecode)", function() {
+        const myCLI = chaiExec('node dist/sourcify.js -c 1 -a "0xfff0f5801a9e13426c306455A3BcC5EF3e9BC979"');
+        chai.expect(myCLI).to.have.stdout.that.contains("perfect");
+        chai.expect(myCLI).exitCode.to.equal(0);
+    })
+    it("should say that address is not found", function() {
         const myCLI = chaiExec('node dist/sourcify.js -c 1 -a "0xfff0f5801a9e13426c306455A3BcC5EF3e9BC978"');
         chai.expect(myCLI).to.have.stdout.that.contains("Address for specified chain not found in repository");
         chai.expect(myCLI).exitCode.to.equal(0);
